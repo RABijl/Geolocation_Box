80363d928e1419e196e940674f7622280fdc01d7
RABijl <r.a.bijl@hotmail.nl>
2021-07-22 12:52:06 +0000

    adds some configs to test serial and debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,21 @@ void setup()
 {
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("led is high");
 
   // wait for a second
   delay(1000);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("led is low");
 
    // wait for a second
   delay(1000);
