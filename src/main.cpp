02ecbc554a57c07c152ecf49f133264a21c4b4cf
RABijl <r.a.bijl@hotmail.nl>
2021-07-22 20:57:12 +0000

    tried out servo library

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,27 @@
 // Set LED_BUILTIN if it is not defined by Arduino framework
 // #define LED_BUILTIN 13
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
-}
+// void setup()
+// {
+//   // initialize LED digital pin as an output.
+//   pinMode(LED_BUILTIN, OUTPUT);
+//   Serial.begin(9600);
+// }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("led is high");
+// // void loop()
+// // {
+// //   // turn the LED on (HIGH is the voltage level)
+// //   digitalWrite(LED_BUILTIN, HIGH);
+// //   Serial.println("led is high");
 
-  // wait for a second
-  delay(1000);
+// //   // wait for a second
+// //   delay(1000);
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("led is low");
+// //   // turn the LED off by making the voltage LOW
+// //   digitalWrite(LED_BUILTIN, LOW);
+// //   Serial.println("led is low");
 
-   // wait for a second
-  delay(1000);
-}
+// //    // wait for a second
+// //   delay(1000);
+// // }
 
