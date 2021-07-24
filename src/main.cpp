21c53b018a434f3eff37974ef465bd4934b36f6f
RABijl <r.a.bijl@hotmail.nl>
2021-07-24 22:28:29 +0000

    adds function to receive serial message

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,15 @@
  * then off for one second, repeatedly.
  */
 #include "Arduino.h"
+#include <Servo.h>
+
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
 #ifdef U8X8_HAVE_HW_I2C
 #include <Wire.h>
 #endif
+
 // Set LED_BUILTIN if it is not defined by Arduino framework
 // #define LED_BUILTIN 13
 
@@ -27,7 +30,7 @@ void loop()
 {
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("led is high");
+  //Serial.println("led is high");
 
   // wait for a second
   delay(1000);
@@ -38,5 +41,24 @@ void loop()
 
    // wait for a second
   delay(1000);
+}
+
+String line = "";
+void serialEvent()
+{
+  
+   while(Serial.available()) 
+   {
+     
+     char ch = Serial.read();
+     if(ch == '\n' ){
+        Serial.println("received this: " +line);
+      line="";
+
+     }else if(ch != 13){
+       line += ch;
+     }
+
+   }
 }
 
